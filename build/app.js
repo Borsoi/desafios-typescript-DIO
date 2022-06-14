3f44b1ffc71f56e75dfa98f7261fbc0861c735c5
Borsoi <borsoi.guilherme@gmail.com>
2022-06-13 21:56:47 -0300

    Resolvido Desafio 1

--- build/app.js
+++ build/app.js
@@ -1,2 +1,5 @@
 "use strict";
-console.log("Hello World");
+let empregado = {
+    code: 10,
+    name: "John"
+};
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+interface IEmployee {
+    code: number,
+    name: string;
+}
+
+let empregado: IEmployee = {
+    code: 10,
+    name: "John"
+};
+
